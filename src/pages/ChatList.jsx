b78d18ad4a5ebd1f36d963432c3a35df08a4636a
Manasa Reddy <kandadimanasareddy12@gmail.com>
2025-09-14 08:54:06 +0530

    Update ChatList.jsx

--- src/pages/ChatList.jsx
+++ src/pages/ChatList.jsx
@@ -5,10 +5,8 @@ import chats from "../data/chats.json";
 export default function ChatList() {
   return (
     <div className="flex flex-col h-screen bg-gray-950 text-gray-200">
-      {/* Navbar with + New Chat button */}
       <Navbar title="Chats" showNew={true} />
 
-      {/* List of chats */}
       <ul className="flex-1 overflow-y-auto divide-y divide-gray-800">
         {chats.map((chat) => (
           <ChatItem key={chat.id} {...chat} />
