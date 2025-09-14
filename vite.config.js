c18cda8b6d54df1692ead749b6646c6fce3a4b26
Manasa Reddy <kandadimanasareddy12@gmail.com>
2025-09-14 08:48:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import history from 'connect-history-api-fallback'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),
-    react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
+  server: {
+    middlewareMode: false,
+    setupMiddlewares(middlewares, devServer) {
+      middlewares.use(
+        history({
+          disableDotRule: true,
+          htmlAcceptHeaders: ['text/html', 'application/xhtml+xml'],
+        })
+      )
+      return middlewares
+    },
+  },
+  preview: {
+    port: 4173,
+    open: true,
+    middlewareMode: false,
+  },
 })
