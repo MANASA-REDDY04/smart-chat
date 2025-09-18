62e395f70ca2c388bdbe1e400b3ed4d5b1fb6ff0
Manasa Reddy <kandadimanasareddy12@gmail.com>
2025-09-18 17:56:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,46 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import ChatList from "./pages/ChatList.jsx";
 import ChatWindow from "./pages/ChatWindow.jsx";
 import NewChat from "./pages/NewChat.jsx";
 
-function App() {
-  return (
-    <Router>
-      <div className="min-h-screen bg-gray-950 text-gray-200 flex flex-col md:flex-row">
-        {/* Chat list always visible on md+ */}
-        <div className="w-full md:w-1/3 border-r border-gray-800">
-          <Routes>
-            <Route path="/" element={<ChatList />} />
-            <Route path="/chat/:id" element={<ChatList />} />
-            <Route path="/new" element={<ChatList />} />
-          </Routes>
-        </div>
+function Layout() {
+  const location = useLocation();
+  const isChatOpen =
+    location.pathname.startsWith("/chat/") || location.pathname.startsWith("/new");
 
-        {/* Chat window / New chat */}
-        <div className="w-full md:flex-1">
-          <Routes>
-            <Route path="/chat/:id" element={<ChatWindow />} />
-            <Route path="/new" element={<NewChat />} />
-          </Routes>
-        </div>
+  return (
+    <div className="min-h-screen bg-gray-950 text-gray-200 flex flex-col md:flex-row">
+
+      {/* LEFT: Chat list */}
+      <div
+        className={`border-r border-gray-800 
+          ${isChatOpen ? "hidden md:block md:w-1/3" : "w-full md:w-1/3"}`}
+      >
+        <Routes>
+          <Route path="/" element={<ChatList />} />
+          <Route path="/chat/:id" element={<ChatList />} />
+          <Route path="/new" element={<ChatList />} />
+        </Routes>
       </div>
-    </Router>
+
+      {/* RIGHT: Chat window or new chat */}
+      <div
+        className={`bg-gray-950
+          ${isChatOpen ? "w-full md:flex-1" : "hidden md:flex-1"}`}
+      >
+        <Routes>
+          <Route path="/chat/:id" element={<ChatWindow />} />
+          <Route path="/new" element={<NewChat />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Layout />
+    </Router>
+  );
+}
